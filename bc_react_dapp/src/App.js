1c61672553cd9c378aa7e39eb8230f5b41c548e3
Piyush Ramavat <piyu.aryan@gmail.com>
2018-03-08 19:29:32 +1100

    Added Web3, ABI and called the transaction on Local Blockchain

--- bc_react_dapp/src/App.js
+++ bc_react_dapp/src/App.js
@@ -2,10 +2,75 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import http from 'http';
+import Web3 from 'web3';
+
+// we're accessing local blockchain
+const web3 = new Web3("http://localhost:8646", http);
+
+const abi = [
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "getId",
+        "outputs": [
+            {
+                "name": "",
+                "type": "string"
+            },
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_fName",
+                "type": "string"
+            },
+            {
+                "name": "_age",
+                "type": "uint256"
+            }
+        ],
+        "name": "setId",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+];
+
+const contractAddress = "0x1647b1461251a0c6d3e14a227de0f16e36ba80b1";
+
+const IdContract = new web3.eth.Contract(abi, contractAddress);
+
 class App extends Component {
     state = {
         firstName: '',
         age: 0
+    };
+
+    _setId = () => {
+        console.log("Clicked.", IdContract);
+
+        // IdContract.methods.setId(this.state.firstName, parseInt(this.state.age));
+
+        // Above line wouldn't update the state on blockchain
+        // to actually push the changes on Blockchain, we need ethers, account, unlock account, And Signing the transaction.
+        // Current test env already has all accounts unlocked
+
+        // So,
+        IdContract.methods.setId(this.state.firstName, parseInt(this.state.age))
+            .send({from: "0xa00af22d07c87d96eeeb0ed583f8f6ac7812827e"})
+            .on("transactionHash", (tx) => console.log(tx))
+            .then(console.log);
     };
 
     render() {
@@ -25,6 +90,9 @@ class App extends Component {
                             <input onChange={(event) => this.setState({age: event.target.value})}/>
                             <p>{this.state.age}</p>
                         </div>
+                    </div>
+                    <div className="Row">
+                        <button onClick={() => this._setId()}> Set Id</button>
                     </div>
                 </div>
             </div>
