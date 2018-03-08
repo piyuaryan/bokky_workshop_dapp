b69cb0318d8338817bed11899e0d1f330781137e
Piyush Ramavat <piyu.aryan@gmail.com>
2018-03-08 19:00:47 +1100

    Added some styling and created HTML structure.

--- bc_react_dapp/src/App.js
+++ bc_react_dapp/src/App.js
@@ -1,21 +1,35 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    state = {
+        firstName: '',
+        age: 0
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">BC-React-Dapp</h1>
+                </header>
+                <div className="Container">
+                    <div className="Row">
+                        <div className="Column">
+                            <input onChange={(event) => this.setState({firstName: event.target.value})}/>
+                            <p>{this.state.firstName}</p>
+                        </div>
+                        <div className="Column">
+                            <input onChange={(event) => this.setState({age: event.target.value})}/>
+                            <p>{this.state.age}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
